4c7acbe4010e87d31ed178d755b0e275190b16b1
raulng9 <salprez4@gmail.com>
2020-11-24 00:28:07 +0100

    Start of name contour search

--- bubbly.py
+++ bubbly.py
@@ -93,11 +93,41 @@ def showExamInformation(correctAnswers, finalFrame):
     testSheetHeight, testSheetWidth, testSheetChannel = finalFrame.shape
     cv2.putText(finalFrame, "Final score: " + "{:.2f}%".format(finalScore), (int(testSheetWidth/5), 30),
     	cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2, bottomLeftOrigin=False)
-    cv2.imshow("Test results", finalFrame)
+    #cv2.imshow("Test results", finalFrame)
     custom_config_tesseract = r'--oem 3 --psm 6'
     print(pytesseract.image_to_string(finalFrame, config=custom_config_tesseract))
 
+def findNameContour(transformedFrame):
+    #Image treatment until edges
+    blurSheetFrame = cv2.GaussianBlur(transformedFrame,(5,5),0)
+    edgesSheetFrame = cv2.Canny(blurSheetFrame, 75,200)
 
+    #Contour treatment
+    contoursSecondPass = cv2.findContours(edgesSheetFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contoursSecondPass = imutils.grab_contours(contoursSecondPass)
+
+    contourOfName = None
+    transformedFrame = cv2.cvtColor(transformedFrame, cv2.COLOR_GRAY2RGB)
+    cv2.drawContours(transformedFrame, contoursSecondPass, -1, (0,255,0),3)
+    #cv2.imshow("jejej", transformedFrame)
+
+    if len(contoursSecondPass) > 0:
+        contoursSorted = sorted(contoursSecondPass, key=cv2.contourArea, reverse=True)
+        print("len")
+        print(len(contoursSorted))
+        for cont in contoursSorted:
+            #Now we look for the biggest four corner polygon
+            perimeter = cv2.arcLength(cont, True)
+            approximatedPoly = cv2.approxPolyDP(cont, 0.02 * perimeter, True)
+            if len(approximatedPoly) == 4:
+                contourOfName = approximatedPoly
+                print("app i got it")
+                break
+
+        if contourOfName is not None:
+         cv2.drawContours(transformedFrame, contourOfName, -1, (0,255,0),3)
+         cv2.imshow("ajaja", transformedFrame)
+         print("got here my man")
 
 #actual work
 def frameScan():
@@ -128,10 +158,13 @@ def frameScan():
                     break
 
         if contourOfTest is not None:
+
             cv2.drawContours(frame, contourOfTest, -1, (0,255,0),3)
             #Perspective transform to isolate the test document
             testFrame = four_point_transform(frame, contourOfTest.reshape(4,2))
             transformedFrame = four_point_transform(greyFrame, contourOfTest.reshape(4,2))
+
+            findNameContour(transformedFrame)
 
             #We apply the threshold to obtain the circle contours
             thresholdFrame = cv2.threshold(transformedFrame, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
