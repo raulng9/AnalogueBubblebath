e79993c14602967165b18e6e95e83c005c2fe5e4
raulng9 <salprez4@gmail.com>
2020-12-12 22:30:35 +0100

    Renamed files and functions, refactored some and cleaned code
    Added a new student to the files as well

--- grader.py
+++ grader.py
@@ -5,6 +5,7 @@ from imutils import contours
 from imutils.perspective import four_point_transform
 import imutils
 import random
+import os
 import sys
 import math
 import pytesseract
@@ -25,13 +26,14 @@ studentImageForDisplay = None
 answersFrame = None
 currentCorrectAnswers = None
 
+keepScanning = True
+
 #primero sort vertical y luego horizontal, luego se splittea en los grupos
 #equivalentes al número de preguntas por fila
-def sortAndGradeAnswers(contoursOfAnswers, referenceFrame, originalFrame):
+def sort_and_grade_answers(contoursOfAnswers, referenceFrame, originalFrame):
     global currentCorrectAnswers
     questionsSortedVertical = contours.sort_contours(contoursOfAnswers, method="top-to-bottom")[0]
     correctAnswers = 0
-    #originalFrameColor = cv2.cvtColor(originalFrame, cv2.COLOR_GRAY2RGB)
     originalFrameForMask = cv2.cvtColor(referenceFrame, cv2.COLOR_BGR2GRAY)
     originalFrameForMask = cv2.threshold(originalFrameForMask, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     for(q, i) in enumerate(np.arange(0, len(questionsSortedVertical), 3)):
@@ -42,37 +44,29 @@ def sortAndGradeAnswers(contoursOfAnswers, referenceFrame, originalFrame):
         for(j,contour) in enumerate(questionsSortedHorizontal):
             mask = np.zeros(originalFrameForMask.shape, dtype="uint8")
             cv2.drawContours(mask, [contour], -1, 255, -1)
-            #cv2.imshow("mascara", mask)
-
             mask = cv2.bitwise_and(originalFrameForMask, originalFrameForMask, mask=mask)
             totalNonZero = cv2.countNonZero(mask)
-
             if filledIn is None or totalNonZero > filledIn[0]:
                 filledIn = (totalNonZero,j)
                 contourForIteration = contour
 
         contoursFilled.append(contourForIteration)
-        # initialize the contour color and the index of the
-	    # *correct* answer
         color = (0, 0, 255)
         k = answers[q]
-
 	    # check to see if the bubbled answer is correct
         if k == filledIn[1]:
             color = (0, 255, 0)
             correctAnswers += 1
-
 	    # draw the outline of the correct answer on the test
         cv2.drawContours(originalFrame, contoursFilled, -1, color, 3)
-    #cv2.imshow("ref", originalFrame)
-    #print("Number of correct answers:")
-    #print(correctAnswers)
+
     currentCorrectAnswers = correctAnswers
-    showExamInformation(correctAnswers, originalFrame)
+    show_exam_information(correctAnswers, originalFrame)
+    print("Test graded")
 
 
 
-def findTestCircles(threshFrame):
+def find_test_circles(threshFrame):
     bubbleContours = cv2.findContours(threshFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     bubbleContours = imutils.grab_contours(bubbleContours)
     bubbleContoursFiltered = []
@@ -86,7 +80,7 @@ def findTestCircles(threshFrame):
     return bubbleContoursFiltered;
 
 
-def getTestDetails(filename):
+def get_test_details(filename):
     testFile = open(filename, "r")
     testData = testFile.read()
     stringWithData = testData.split()
@@ -95,7 +89,7 @@ def getTestDetails(filename):
     questionsPerRow = listOfMappedData[0]
     for i in range(0,len(listOfMappedData)-1):
         answers[i]=listOfMappedData[i+1]
-    print(answers)
+    print("Test answers loaded")
 
 def truncate(number, decimals=0):
     if not isinstance(decimals, int):
@@ -112,21 +106,19 @@ def load_student_image():
     global studentImageForDisplay
     pathForStudent = "StudentPictures/" + currentStudent + ".jpg"
     studentImg = cv2.imread(pathForStudent, cv2.IMREAD_UNCHANGED)
-    #cv2.imshow("stu",studentImg)
     width = 100
     height = 100
     dim = (width, height)
     resizedImg = cv2.resize(studentImg, dim, interpolation = cv2.INTER_AREA)
     studentImageForDisplay = resizedImg
-    #cv2.imshow("emma", resizedImg)
 
 def display_student_image():
     x_offset=y_offset=10
     answersFrame[y_offset:y_offset+studentImageForDisplay.shape[0], x_offset:x_offset+studentImageForDisplay.shape[1]] = studentImageForDisplay
-    cv2.destroyWindow("Test results")
+    cv2.destroyAllWindows()
     cv2.imshow("Results and student", answersFrame)
 
-def showExamInformation(correctAnswers, finalFrame):
+def show_exam_information(correctAnswers, finalFrame):
     global answersFrame
     if finalFrame is None:
         return
@@ -143,29 +135,25 @@ def showExamInformation(correctAnswers, finalFrame):
         answersFrame = finalFrame
         display_student_image()
     else:
+        cv2.destroyAllWindows()
         cv2.imshow("Test results", finalFrame)
     answersFrame = finalFrame
 
 
-def findNameContour(transformedFrame):
+def find_name_contour(transformedFrame):
     if currentStudent != "" and answersFrame is not None:
         return
-    #transformedFrame = cv2.rotate(transformedFrame, cv2.ROTATE_180)
     #Image treatment until edges
     blurSheetFrame = cv2.GaussianBlur(transformedFrame,(5,5),0)
     edgesSheetFrame = cv2.Canny(blurSheetFrame, 75,200)
-    #cv2.imshow("edgesFrame", edgesSheetFrame)
     #Contour treatment
     contoursSecondPass = cv2.findContours(edgesSheetFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     contoursSecondPass = imutils.grab_contours(contoursSecondPass)
 
     contourOfName = None
-    #cv2.drawContours(transformedFrame, contoursSecondPass, -1, (255,0,0),3)
-    #cv2.imshow("contoursFirstPass", transformedFrame)
+
     if len(contoursSecondPass) > 0:
         contoursSorted = sorted(contoursSecondPass, key=cv2.contourArea, reverse=True)
-        #print("len")
-        #print(len(contoursSorted))
         for cont in contoursSorted:
             #Now we look for the biggest four corner polygon
             perimeter = cv2.arcLength(cont, True)
@@ -177,11 +165,9 @@ def findNameContour(transformedFrame):
             cv2.drawContours(transformedFrame, contourOfName, -1, (0,255,0),3)
             heightOfCropBR = transformedFrame.shape[1]
             widthOfCropBR = transformedFrame.shape[0]
-            #bottomRightCoordName = [heightOfCropBR,widthOfCropBR]
             #we pull out the top left coordinate
             topLeftCoordName = contourOfName[1][0].tolist()
             bottomRightCoordName = contourOfName[3][0].tolist()
-            #print([topLeftCoordName, bottomRightCoordName])
 
             topLeftExtra = [topLeftCoordName[0]+50, topLeftCoordName[1]]
             bottomRightExtra = [bottomRightCoordName[0]-30,bottomRightCoordName[1]]
@@ -189,45 +175,29 @@ def findNameContour(transformedFrame):
             topLeftCoordNoName= contourOfName[2][0].tolist()
             bottomRightCoordNoName = [heightOfCropBR,widthOfCropBR]
 
-            print("----------")
-
             #the actual cropping done by slicing the frame from corner to corner (y coords go first (!))
             frameNameBox = transformedFrame[topLeftCoordName[1]:bottomRightCoordName[1], topLeftCoordName[0]:bottomRightCoordName[0]]
             frameNameBoxExtra = transformedFrame[topLeftExtra[1]:bottomRightExtra[1], topLeftExtra[0]:bottomRightExtra[0]]
 
             frameWithoutNameBox = transformedFrame[topLeftCoordNoName[1]:bottomRightCoordNoName[1], topLeftCoordNoName[0]:bottomRightCoordNoName[0]]
 
-            # if frameNameBox.shape[0] > 0 and frameNameBox.shape[1] > 0:
-            #     a = 1
-            #     frameNameBox = cv2.rotate(frameNameBox,cv2.ROTATE_180)
-            #     #cv2.imshow("cuadroDeNombre", frameNameBox)
-            #     custom_config_tesseract = r'--oem 3 --psm 6'
-            #     #print(pytesseract.image_to_string(frameNameBox, config=custom_config_tesseract))
             if frameNameBoxExtra.shape[0] > 0 and frameNameBoxExtra.shape[1] > 0:
-                a = 1
-                #frameNameBoxExtra = cv2.rotate(frameNameBoxExtra,cv2.ROTATE_180)
-                #cv2.imshow("cuadroDeNombre", frameNameBoxExtra)
                 custom_config_tesseract = r'--oem 3 --psm 6'
                 nameFoundOCR = pytesseract.image_to_string(frameNameBoxExtra, config=custom_config_tesseract)
                 alphaNameFound = ''.join(filter(str.isalpha, nameFoundOCR))
                 check_for_student(alphaNameFound)
-                print(alphaNameFound)
+
+
             if frameWithoutNameBox.shape[0] > 0 and frameWithoutNameBox.shape[1] > 0:
-                a = 1
-                #frameWithoutNameBox = cv2.rotate(frameWithoutNameBox,cv2.ROTATE_180)
-                #cv2.imshow("cuadroDeNombre", frameWithoutNameBox)
                 frameWithoutNameBoxColor = frameWithoutNameBox.copy()
                 frameWithoutNameBoxColorCopy = frameWithoutNameBoxColor.copy()
                 frameWithoutNameBox = cv2.cvtColor(frameWithoutNameBox, cv2.COLOR_BGR2GRAY)
                 #We apply the threshold to obtain the circle contours
                 frameWithoutNameBox = cv2.threshold(frameWithoutNameBox, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-                #cv2.imshow("aa", thresholdFrame)
-                contoursOfOptions = findTestCircles(frameWithoutNameBox)
-                #cv2.drawContours(frameWithoutNameBox, contoursOfOptions, -1, (255,0,0),3)
-                #cv2.imshow("bubbleContours",frameWithoutNameBox)
+                contoursOfOptions = find_test_circles(frameWithoutNameBox)
                 if contoursOfOptions is not None:
                     if len(contoursOfOptions) == 9:
-                        questionsFullySorted = sortAndGradeAnswers(contoursOfOptions,frameWithoutNameBoxColor, frameWithoutNameBoxColorCopy)
+                        questionsFullySorted = sort_and_grade_answers(contoursOfOptions,frameWithoutNameBoxColor, frameWithoutNameBoxColorCopy)
 
 
 def check_for_student(studentName):
@@ -235,70 +205,39 @@ def check_for_student(studentName):
     global averageForCurrentStudent
     for student in listOfStudents:
         if student.name == studentName:
+            print("Student found")
             currentStudent = student.name
             averageForCurrentStudent = sum(student.listOfMarks)/len(student.listOfMarks)
-            showExamInformation(currentCorrectAnswers, answersFrame)
-#actual work
-def frameScan():
-    while(True):
+            show_exam_information(currentCorrectAnswers, answersFrame)
+
+
+#actual frame retrieval and management
+def frame_scan():
+    #infinite loop that retrieves the video frame
+    while(keepScanning):
         ret, frame = inputFromWebcam.read()
         frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
-        #Image treatment until edges
-        greyFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurFrame = cv2.GaussianBlur(greyFrame,(5,5),0)
-        edgesFrame = cv2.Canny(blurFrame, 75,200)
 
-
-        #Contour treatment
-        contoursFirstPass = cv2.findContours(edgesFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        contoursFirstPass = imutils.grab_contours(contoursFirstPass)
-
-        contourOfTest = None
         contoursOfOptions = None
         transformedFrame = None
 
-        findNameContour(frame)
+        find_name_contour(frame)
 
-        # if len(contoursFirstPass) > 0:
-        #     contoursSorted = sorted(contoursFirstPass, key=cv2.contourArea, reverse=True)
-        #     for cont in contoursSorted:
-        #         #Now we look for the biggest four corner polygon
-        #         perimeter = cv2.arcLength(cont, True)
-        #         approximatedPoly = cv2.approxPolyDP(cont, 0.02 * perimeter, True)
-        #         if len(approximatedPoly) == 4:
-        #             contourOfTest = approximatedPoly
-        #             break
-        #
-        # if contourOfTest is not None:
-        #
-        #     cv2.drawContours(frame, contourOfTest, -1, (0,255,0),3)
-        #     #Perspective transform to isolate the test document
-        #     testFrame = four_point_transform(frame, contourOfTest.reshape(4,2))
-        #     transformedFrame = four_point_transform(greyFrame, contourOfTest.reshape(4,2))
-        #
-        #     #findNameContour(transformedFrame)
-        #
-        #     #We apply the threshold to obtain the circle contours
-        #     thresholdFrame = cv2.threshold(transformedFrame, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        #     #cv2.imshow("aa", thresholdFrame)
-        #     contoursOfOptions = findTestCircles(thresholdFrame)
-        #
-        #     if contoursOfOptions is not None:
-        #         if len(contoursOfOptions) == 9:
-        #             a = 2
-        #             #questionsFullySorted = sortAndGradeAnswers(contoursOfOptions,thresholdFrame, transformedFrame)
-
+        #to close the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+
 def main():
     global listOfStudents
-    getTestDetails(sys.argv[1])
+    get_test_details(sys.argv[1])
     listOfStudents = retrieve_students()
-    frameScan()
+    frame_scan()
+
 
 if __name__ == "__main__":
     main()
+
 
 inputFromWebcam.release()
 cv2.destroyAllWindows()
